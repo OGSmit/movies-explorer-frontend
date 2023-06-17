b6f1ca55045e0c7c57ab469e4e2ed8ce44f66c10
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-17 12:55:22 +0300

    создал CRA + написал footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer() {
+
+  return (
+    <div className="footer">
+      <p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__container'>
+        <p className="footer__year">© 2023</p>
+        <nav className="footer__nav">
+          <a href="https://practicum.yandex.ru" className="footer__navLink">Яндекс.Практикум</a>
+          <a href="https://github.com/OGSmit" className="footer__navLink">Github</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
