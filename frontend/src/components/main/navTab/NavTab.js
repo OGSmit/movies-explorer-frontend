91ff4d51ce291cb5db9450de17fa16976b60a96a
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-18 13:38:43 +0300

    сделал адаптив для main

--- frontend/src/components/main/navTab/NavTab.js
+++ frontend/src/components/main/navTab/NavTab.js
@@ -5,7 +5,7 @@ function NavTab() {
   return (
     <section className="navTab">
       <nav className="navTab__list">
-        <a href="#aboutProject" className="navTab__item">О проекте</a>
+        <a href="#aboutProject" className="navTab__item">О&nbsp;проекте</a>
         <a href="#techs" className="navTab__item">Технологии</a>
         <a href="#aboutMe" className="navTab__item">Студент</a>
       </nav>
