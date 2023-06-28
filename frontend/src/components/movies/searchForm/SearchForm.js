2f0e83f4032e29959da4bfedcc67ecb5f2a137ec
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-28 17:03:11 +0300

    исправил search-form

--- frontend/src/components/movies/searchForm/SearchForm.js
+++ frontend/src/components/movies/searchForm/SearchForm.js
@@ -7,12 +7,12 @@ function SearchForm() {
       <form className='search-form__container'>
         <input placeholder='Фильм' className='search-form__input'></input>
         <button className='search-form__button'>Найти</button>
+        <div className='search-form__checkbox-container'>
+          <input type="checkbox" id="search-form__checkbox" className='search-form__checkbox'></input>
+          <label htmlFor="search-form__checkbox"></label>
+          <p className='search-form__checkbox-description'>Короткометражки</p>
+        </div>
       </form>
-      <div className='search-form__checkbox-container'>
-        <input type="checkbox" id="search-form__checkbox"  className='search-form__checkbox'></input>
-        <label htmlFor="search-form__checkbox"></label>
-        <p className='search-form__checkbox-description'>Короткометражки</p>
-      </div>
     </section>
   )
 }
