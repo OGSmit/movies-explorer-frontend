6d29641797159696a7f44c541a037e1382848e57
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-29 14:30:17 +0300

    search-form__input добавил атрибут required

--- frontend/src/components/movies/searchForm/SearchForm.js
+++ frontend/src/components/movies/searchForm/SearchForm.js
@@ -5,8 +5,8 @@ function SearchForm() {
   return (
     <section className='search-form'>
       <form className='search-form__container'>
-        <input placeholder='Фильм' className='search-form__input'></input>
-        <button className='search-form__button'>Найти</button>
+        <input required placeholder='Фильм' className='search-form__input'></input>
+        <button type='button' className='search-form__button'>Найти</button>
         <div className='search-form__checkbox-container'>
           <input type="checkbox" id="search-form__checkbox" className='search-form__checkbox'></input>
           <label htmlFor="search-form__checkbox"></label>
