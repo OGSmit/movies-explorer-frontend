8db40e1b78c03fa460cdbd87ee0a73d949d9d5fc
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-29 16:24:47 +0300

    стилизовал input'ы по классам

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -2,7 +2,7 @@ import './Profile.css'
 import Header from '../Header';
 import { useState } from 'react';
 
-function Profile() {
+function Profile({ isloggedIn }) {
   const [formValue, setFormValue] = useState({});
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -34,33 +34,33 @@ function Profile() {
 
   return (
     <section className='profile'>
-      <Header />
+      <Header isloggedIn={isloggedIn} />
       <div className='profile__container'>
         <h2 className='profile__title'>Привет, Виталий!</h2>
         <form className='profile__form'>
           <div className='profile__container-input'>
             <label className='profile__input-label' for='profile__input_name'>Имя</label>
             <input name='name'
-                   onChange={handleChangeName}
-                   type='text'
-                   required
-                   minLength={2}
-                   maxLength={18}
-                   className='profile__input profile__input_name'
-                   placeholder='Виталий'
-                   id='profile__input_name'></input>
-            <span className={formErrorMessage.name === 'undefined'? 'profile__error-invisible': 'profile__error'}>{formErrorMessage.name || ''}</span>
+              onChange={handleChangeName}
+              type='text'
+              required
+              minLength={2}
+              maxLength={18}
+              className='profile__input profile__input_name'
+              placeholder='Виталий'
+              id='profile__input_name'></input>
+            <span className={formErrorMessage.name === 'undefined' ? 'profile__error-invisible' : 'profile__error'}>{formErrorMessage.name || ''}</span>
           </div>
           <div className='profile__container-input'>
             <label className='profile__input-label' for='profile__input_email'>E-mail</label>
             <input name='email'
-                   type='email'
-                   required
-                   onChange={handleChangeEmail}
-                   className='profile__input profile__input_email'
-                   placeholder='pochta@yandex.ru'
-                   id='profile__input_email'></input>
-            <span className={formErrorMessage.email === 'undefined'?  'profile__error-invisible': 'profile__error'}>{formErrorMessage.email || ''}</span>
+              type='email'
+              required
+              onChange={handleChangeEmail}
+              className='profile__input profile__input_email'
+              placeholder='pochta@yandex.ru'
+              id='profile__input_email'></input>
+            <span className={formErrorMessage.email === 'undefined' ? 'profile__error-invisible' : 'profile__error'}>{formErrorMessage.email || ''}</span>
           </div>
           <button type='submit' className='profile__button-submit'>Редактировать</button>
         </form>
