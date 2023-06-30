a6e90af2453c5e65d44eb720758dabbb73b71ba1
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-06-30 14:36:37 +0300

    бэм + адаптив profile

--- frontend/src/components/burgerMenu/BurgerMenu.js
+++ frontend/src/components/burgerMenu/BurgerMenu.js
@@ -3,12 +3,12 @@ import { NavLink, Link, useLocation } from 'react-router-dom';
 function BurgerMenu({ isOpened , onClose }) {
 const location = useLocation()
   return (
-    <div className={isOpened ? 'burger-menu' : 'burger-menu_close'}>
+    <div className={isOpened ? 'burger-menu' : 'burger-menu burger-menu_none'}>
       <button onClick={onClose} className='burger-menu__button-close' type='button'></button>
       <nav className='burger-menu__nav'>
-        <NavLink to='/' className={location.pathname === '/' ? 'burger-menu_link-active' : 'burger-menu_link'}>Главная</NavLink>
-        <NavLink to='/movies' className={location.pathname === '/movies' ? 'burger-menu_link-active' : 'burger-menu_link'}>Фильмы</NavLink>
-        <NavLink to='/saved-movies' className={location.pathname === '/saved-movies' ? 'burger-menu_link-active' : 'burger-menu_link'}>Сохраненые фильмы</NavLink>
+        <NavLink to='/' className={location.pathname === '/' ? 'burger-menu__link-active' : 'burger-menu__link'}>Главная</NavLink>
+        <NavLink to='/movies' className={location.pathname === '/movies' ? 'burger-menu__link-active' : 'burger-menu__link'}>Фильмы</NavLink>
+        <NavLink to='/saved-movies' className={location.pathname === '/saved-movies' ? 'burger-menu__link-active' : 'burger-menu__link'}>Сохраненые фильмы</NavLink>
       </nav>
         <div className='burger-menu__account-info'>
           <p className='burger-menu__account-name'>Аккаунт</p>
