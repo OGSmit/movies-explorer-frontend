46d825aaeb098dd905a1d6dea6aaac60dabf20f5
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-07-16 19:53:53 +0300

    доработал profile

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,18 +1,24 @@
 import './Profile.css'
 import Header from '../Header';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function Profile({ goExit, isloggedIn, onUserEdit, setInfoTool, closeInfoTool }) {
   const userInfo = useContext(CurrentUserContext);
 
-  const [formValue, setFormValue] = useState({});
+  const [formValue, setFormValue] = useState({ name: userInfo.data.name, email: userInfo.data.email });
   const [formErrorMessage, setFormErrorMessage] = useState({});
   const [gonnaEdit, setGonnaEdit] = useState(false);
   const [isDisabledInput, setIsDisabledInput] = useState(true);
-  const [editError, setEditError] = useState('')
-
+  const [editError, setEditError] = useState('');
+  const [isSubmitButtonActive, setIsSubmitButtonActive] = useState(true);
   const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    if (formValue.name === userInfo.data.name && formValue.email === userInfo.data.email) {
+      setIsSubmitButtonActive(true)
+    } else setIsSubmitButtonActive(false)
+  }, [formValue, userInfo])
 
   function handleChangeName(e) {
     const { name, value } = e.target;
@@ -58,11 +64,12 @@ function Profile({ goExit, isloggedIn, onUserEdit, setInfoTool, closeInfoTool })
     onUserEdit(name, email)
       .then(() => setInfoTool({ text: 'Успешно', statusOk: true, opened: true }))
       .catch((err) => {
-        setInfoTool({text: err, statusOk:false, opened: true })
-        (err === 'Ошибка: 409') ?
+        setInfoTool({ text: err, statusOk: false, opened: true })
+          (err === 'Ошибка: 409') ?
           setEditError('Пользователь с таким email уже существует.') : setEditError('При регистрации пользователя произошла ошибка.')
       })
       .finally(() => setIsDisabledInput(false));
+
   }
 
   return (
@@ -76,13 +83,13 @@ function Profile({ goExit, isloggedIn, onUserEdit, setInfoTool, closeInfoTool })
               <label className='profile__input-label' htmlFor='profile__input_name'>Имя</label>
               <input name='name'
                 disabled={isDisabledInput}
+                value={formValue.name}
                 onChange={handleChangeName}
                 type='text'
                 required
                 minLength={2}
                 maxLength={18}
                 className='profile__input profile__input_name'
-                placeholder={userInfo.data.name}
                 id='profile__input_name'></input>
               <span className={formErrorMessage.name === 'undefined' ? 'profile__error-invisible' : 'profile__error'}>{formErrorMessage.name || ''}</span>
             </div>
@@ -90,18 +97,18 @@ function Profile({ goExit, isloggedIn, onUserEdit, setInfoTool, closeInfoTool })
               <label className='profile__input-label' htmlFor='profile__input_email'>E-mail</label>
               <input name='email'
                 disabled={isDisabledInput}
+                value={formValue.email}
                 type='email'
                 required
                 onChange={handleChangeEmail}
                 className='profile__input profile__input_email'
-                placeholder={userInfo.data.email}
                 id='profile__input_email'></input>
               <span className={formErrorMessage.email === 'undefined' ? 'profile__error-invisible' : 'profile__error'}>{formErrorMessage.email || ''}</span>
             </div>
             {gonnaEdit ?
               <>
                 <span className={editError.length > 1 ? 'profile__submit-error' : 'profile__submit-error profile__submit-error_invisible'}>{editError}</span>
-                <button onClick={handleUserEdit} disabled={isDisabledInput && !isFormValid} type='button' className='profile__button-save'>Сохранить</button>
+                <button onClick={handleUserEdit} disabled={isDisabledInput || !isFormValid || isSubmitButtonActive} type='button' className='profile__button-save'>Сохранить</button>
               </> :
               <button onClick={handelGonnaEdit} type='button' className='profile__button-edit'>Редактировать</button>
             }
